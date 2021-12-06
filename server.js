7642c1d19fac52d5273bc3084d29ddfa2ef53132
DiegoGarcia0319 <diegobioingeniero>
2021-12-06 13:48:33 -0500

    deploy

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the distdirectory
-app.use(express.static(__dirname+ '/dist/app_contable_frontend'));
+app.use(express.static(__dirname+ '/dist/appContable'));
 
 // Send all request to index.html
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/app_contable_frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/appContable/index.html'));
 });
 // Start the app by listening on the default Herokuport
 app.listen(process.env.PORT|| 5000);
